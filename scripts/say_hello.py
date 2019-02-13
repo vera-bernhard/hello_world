3075fa09ed82e4864ac91b494a4cfd0683e34532
mt2019 <errare@hispeed.ch>
2019-02-13 14:14:21 +0100

    added say_hello.py

---
+++ scripts/say_hello.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+def hello_world():
+    return 'Hello World'
+
+def hello_universe():
+    return 'Hello Universe'
+
+def main():
+    print(hello_universe())
+
+if __name__ == '__main__':
+    main()
